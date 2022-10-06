5437b05c8cc614a795da6b3937e6962dff70b545
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-10-06 12:37:17 +0100

    added the questions and answers for the quiz

---
+++ assets/js/scripts.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        questionText: "Inside which HTML element do we put the js?",
+        options: ["1. <js>", "2. <script>", "3. <JavaScript>", "4. <scripting>"],
+        answer: "2. <script>",
+    },
+    {
+        questionText: "Where is the correct place to insert a JavaScript?",
+        options: ["1. Both the <head> section and the <body> section are correct", "2. The <body> section", "3.The <head>  section"],
+        answer: "1. Both the <head> section and the <body> section are correct",
+    },
+    {
+        questionText: "How do you write Hello World in an alert box?",
+        options: ["1. alertBox(“Hello World”);", "2. Msg(“Hello World”);", "3. Alert(“Hello World”);", "4. msgBox(“Hello World”);"],
+        answer: "3. Alert(“Hello World”);",
+    },
+    {
+        questionText: "How does a FOR loop start?",
+        options: ["1. For (I = 0; I <=5; I++)", "2. For (I <= 5; I++)", "3. For (I = 0; I <=5)", "4. For I = 1 to 5"],
+        answer: "1. For (I = 0; I <=5; I++)",
+    },
+    {
+        questionText: "How can you add a comment in a JavaScript?",
+        options: ["1. ‘This is a comment", "2. <!—This is a comment —>", "3. //This is a comment"],
+        answer: "3. //This is a comment",
+    },
+    {
+        questionText: "Which event occurs when the user clicks on an HTML element?",
+        options: ["1. onclick", "2. onmouseover", "3. onmouseclick", "4. onchange"],
+        answer: "1. onclick",
+    },
+    {
+        questionText: "Which operator is used to assign a value to a variable?",
+        options: ["1. -", "2. *", "3. =", "4. X"],
+        answer: "3. =",
+    },
+
+]
