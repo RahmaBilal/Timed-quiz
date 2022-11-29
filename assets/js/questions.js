4f6940b38455894f6ff97c13c13411a4893047eb
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-11-29 23:35:52 +0000

    correcting questions and making them visible on webpage

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,21 @@
 const questions = [
     {
       questionText: "Inside which HTML element do we put the js?",
-      options: ["1. <js>", "2. <script>", "3. <JavaScript>", "4. <scripting>"],
-      answer: "2. <script>",
+      options: [
+        "1. js",
+        "2. script",
+        "3. JavaScript",
+        "4. scripting"],
+      answer: "2. script",
     },
     {
       questionText: "Where is the correct place to insert a JavaScript?",
       options: [
-        "1. Both the <head> section and the <body> section are correct",
-        "2. The <body> section",
-        "3.The <head>  section",
+        "1. Both the head section and the body section are correct",
+        "2. The body section",
+        "3.The head  section",
       ],
-      answer: "1. Both the <head> section and the <body> section are correct",
+      answer: "1. Both the head section and the body section are correct",
     },
     {
       questionText: "How do you write Hello World in an alert box?",
@@ -37,7 +41,7 @@ const questions = [
       questionText: "How can you add a comment in a JavaScript?",
       options: [
         "1. ‘This is a comment",
-        "2. <!—This is a comment —>",
+        "2. !—This is a comment —!",
         "3. //This is a comment",
       ],
       answer: "3. //This is a comment",
